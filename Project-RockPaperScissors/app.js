8543a889b54ebc83f2260852cdef45c0fc760d82
OzerOzturk <ozerozturk1636@gmail.com>
2021-03-09 22:18:41 +0300

    functions, conditionals, logic and so on. project has an effective work now. ready to play.

---
+++ Project-RockPaperScissors/app.js
@@ -0,0 +1,119 @@
+// Necessary DOM nodes
+const images = Array.from(document.querySelectorAll('.card-image'));
+const message = document.querySelector('.message');
+const scorePlayer = document.querySelector('.player-score');
+const scoreComputer = document.querySelector('.computer-score');
+const choiceYour = document.querySelector('.your-choice');
+const choiceComputer = document.querySelector('.computer-choice');
+
+let playerScore = 0;
+let computerScore = 0;
+
+// Start the game when user clicks at an image
+images.forEach((image) =>
+  image.addEventListener('click', () => {
+    if (playerScore >= 5 || computerScore >= 5) {
+      return;
+    }
+    game(image.dataset.image);
+  })
+);
+
+/* Logical things of the game, function and conditionals */
+
+function getComputerSelection() {
+    let computerNumber = random(3);
+    let computerGuess = '';
+  
+    switch (computerNumber) {
+      case 1:
+        computerGuess = 'Rock';
+        break;
+      case 2:
+        computerGuess = 'Paper';
+        break;
+      case 3:
+        computerGuess = 'Scissors';
+        break;
+      default:
+        break;
+    }
+  
+    return computerGuess;
+  }
+
+  function playRound(playerSelection, computerSelection) {
+    let log = '';
+  
+    if (playerSelection === 'Rock') {
+      if (computerSelection === 'Paper') {
+        log = 'You Lose! Paper beats Rock';
+      } else if (computerSelection === 'Scissors') {
+        log = 'You Win! Rock beats Scissors';
+      } else {
+        log = "It's a draw";
+      }
+    } else if (playerSelection === 'Paper') {
+      if (computerSelection === 'Scissors') {
+        log = 'You Lose! Scissors beats Paper';
+      } else if (computerSelection === 'Rock') {
+        log = 'You Win! Paper beats Rock';
+      } else {
+        log = "It's a draw";
+      }
+    } else if (playerSelection === 'Scissors') {
+      if (computerSelection === 'Rock') {
+        log = 'You Lose! Rock beats Scissors';
+      } else if (computerSelection === 'Paper') {
+        log = 'You Win! Scissors beats Paper';
+      } else {
+        log = "It's a draw";
+      }
+    }
+  
+    return log;
+  }
+
+  function createParagWithText(text) {
+    const p = document.createElement('p');
+    p.textContent = text;
+  
+    return p;
+  }
+  
+  function game(playerSelect) {
+    let playerSelection = capitalize(playerSelect);
+    let computerSelection = getComputerSelection();
+  
+    let roundResult = playRound(playerSelection, computerSelection);
+  
+    if (roundResult.search('You Win!') > -1) {
+      playerScore++;
+    } else if (roundResult.search('You Lose!') > -1) {
+      computerScore++;
+    }
+  
+    scorePlayer.textContent = playerScore;
+    scoreComputer.textContent = computerScore;
+    message.textContent = roundResult;
+    choiceYour.appendChild(createParagWithText(playerSelection));
+    choiceComputer.appendChild(createParagWithText(computerSelection));
+  
+    if (playerScore >= 5 && computerScore < 5) {
+      message.textContent = 'You Win, Well Done!';
+    } else if (playerScore < 5 && computerScore >= 5) {
+      message.textContent = 'Game Over, You Lose!';
+    }
+  }
+  
+  /* Helper Functions */
+  function random(number) {
+    return Math.floor(Math.random() * number + 1);
+  }
+  
+  function capitalize(string) {
+    return (
+      string.toLowerCase().charAt(0).toUpperCase() + string.toLowerCase().slice(1)
+    );
+  }
+
