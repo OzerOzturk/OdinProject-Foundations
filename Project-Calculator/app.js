8cff9744d6ca05a14143e6b4b05747e9ea314fae
OzerOzturk <ozerozturk1636@gmail.com>
2021-04-28 23:19:26 +0300

    necessary variables and four operations functions with operate function.

--- Project-Calculator/app.js
+++ Project-Calculator/app.js
@@ -1,27 +1,49 @@
+let numberButtons = document.querySelectorAll('.number-btn');
+let operatorButtons = document.querySelectorAll('.operator-btn');
+let totalButton = document.querySelector('#total-btn');
+let deleteButton = document.querySelector('#delete-btn');
+let clearButton = document.querySelector('#clear-btn');
+let decimalButton = document.querySelector('#decimal-btn');
+let display = document.querySelector('#number-display');
 
-let x
-let y
+/* four operations functions */
 
-
-
-
-function add(x,y){
-    return x+y;
+function add(num1, num2) {
+    return num1 + num2;
 }
 
-function subtract(x,y){
-    return x-y;
+function subtract(num1, num2) {
+    return num1 - num2;
 }
 
-function multiply(x,y){
-    return x*y;
+function multiply(num1, num2) {
+    return num1 * num2;
 }
 
-function divide(x,y){
-    return x/y;
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+/* other functions */
+
+function operate(num1, num2, operator) {
+    if ((num1 == 0 || num2 == 0) && operator == divide) {
+        return 'Error';
+    }
+    return operator(num1, num2);
 }
 
 
-function operate(x,y){
-   
+function checkOperator(string) {
+    switch (string) {
+        case 'add':
+            return add;
+        case 'subtract':
+            return subtract;
+        case 'multiply':
+            return multiply;
+        case 'divide':
+            return divide;
+    }
 }
+
