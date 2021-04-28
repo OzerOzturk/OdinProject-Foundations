86549546f1f0822e0ca42501d60fb9005704098e
OzerOzturk <ozerozturk1636@gmail.com>
2021-04-28 21:30:10 +0300

    initial code for calculator

---
+++ Project-Calculator/app.js
@@ -0,0 +1,25 @@
+
+let x
+let y
+
+
+function sum(x,y){
+    return x+y;
+}
+
+function subtract(x,y){
+    return x-y;
+}
+
+function multiply(x,y){
+    return x*y;
+}
+
+function divide(x,y){
+    return x/y;
+}
+
+
+function operate(x,y){
+    return x-y;
+}
